233cf29b3121e7dc6a11d1f0b591163e5d146a3b
HelenArkh <alena.arkh@gmail.com>
2021-03-30 22:12:23 +0300

    Выполнена практика

---
+++ Module16Practice/Module16Practice.Tests/CalculatorTests .cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16Practice.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 220), Is.EqualTo(230));
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(10, 5), Is.EqualTo(50));
+        }
+    }
+}
